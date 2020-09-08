4f41f97fa35397d5fa83843e49932e1880fb7ed1
Mike <mzeno184@gmail.com>
2020-09-07 22:25:04 -0400

    Created state and ref for tasks

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
 import './App.css';
 
 export default function Tasks() {
+    const [tasks, setTasks] = useState([]);
+    const taskRef = useRef();
+
+    const addTask = () => {
+        console.log('Add task button clicked = ' + taskRef.current.value);
+        setTasks([...tasks, taskRef.current.value]);
+    }
+
     return (
         <div>
             <h1>Tasks</h1>
             <form>
-                <input type="text" id="taskDesc" placeholder="Task..." />
-                <button type="button" id="taskBtn">Add Task</button>
+                <input type="text" id="taskDesc" ref={taskRef} placeholder="Task..." />
+                <button type="button" id="taskBtn" onClick={addTask}>Add Task</button>
             </form>
             <label>Show:</label>
             <ul id="filter">
@@ -19,7 +27,9 @@ export default function Tasks() {
                 <tbody>
                     <tr>
                         <td><input type="checkbox" /></td>
-                        <td>Task Desc</td>
+                        <td>{tasks.map((value, index) => (
+                            <div key={index}>{value}</div>
+                        ))}</td>
                         <td><button type="button" id="editBtn">Edit</button></td>
                         <td><button type="button" id="deleteBtn">X</button></td>
                     </tr>
