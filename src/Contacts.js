a93e274c2388dbbfc2af42814da2d488b0f586ba
Mike <mzeno184@gmail.com>
2020-09-07 21:02:26 -0400

    Added contact form structure

--- src/Contacts.js
+++ src/Contacts.js
@@ -5,6 +5,21 @@ export default function Contacts() {
     return (
         <div>
             <h1>Contact</h1>
+            <form>
+                <formfield>
+                    <input id="fname" type="text" placeholder="First Name" />
+                </formfield>
+                <formfield>
+                    <input id="lname" type="text" placeholder="Last Name" />
+                </formfield>
+                <formfield>
+                    <input id="email" type="email" placeholder="Email" />
+                </formfield>
+                <formfield>
+                    <input id="comment" type="text" placeholder="Comments (optional)" />
+                </formfield>
+                <button id="submitBtn" type="button" onClick="">SAVE</button>
+            </form>
         </div>
     )
 }
