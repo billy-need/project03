afc747af4d9dcc47471cc63aaedba7ce43fa5b61
Mike <mzeno184@gmail.com>
2020-09-07 21:06:40 -0400

    Corrected contact form structure

--- src/Contacts.js
+++ src/Contacts.js
@@ -2,23 +2,19 @@ import React from 'react'
 import './App.css';
 
 export default function Contacts() {
+    const handleClick = () => {
+        console.log('Contact submit button clicked');
+    }
+
     return (
         <div>
             <h1>Contact</h1>
             <form>
-                <formfield>
-                    <input id="fname" type="text" placeholder="First Name" />
-                </formfield>
-                <formfield>
-                    <input id="lname" type="text" placeholder="Last Name" />
-                </formfield>
-                <formfield>
-                    <input id="email" type="email" placeholder="Email" />
-                </formfield>
-                <formfield>
-                    <input id="comment" type="text" placeholder="Comments (optional)" />
-                </formfield>
-                <button id="submitBtn" type="button" onClick="">SAVE</button>
+                <input id="fname" type="text" placeholder="First Name" />
+                <input id="lname" type="text" placeholder="Last Name" />
+                <input id="email" type="email" placeholder="Email" />
+                <input id="comment" type="text" placeholder="Comments (optional)" />
+                <button id="submitBtn" type="button" onClick={handleClick}>SAVE</button>
             </form>
         </div>
     )
