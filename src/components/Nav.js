06efaa8d5bafc6334d0cf548be3bae73dd8fd61a
Mike <mzeno184@gmail.com>
2020-09-10 15:20:37 -0400

    Updated Nav and Css styling

---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../App.css';
+import {Link} from 'react-router-dom'
+
+export default function Nav() {
+    return (
+        <nav>
+            <h3>Daily Planner</h3>
+            <ul className="nav justify-content-center">
+                <Link style={{textDecoration: 'none'}} to="/"><li className="nav-item nav-link active">Home</li></Link>
+                <Link style={{textDecoration: 'none'}} to="/components/tasks"><li className="nav-item nav-link">Tasks</li></Link>
+                <Link style={{textDecoration: 'none'}} to="/components/contacts"><li className="nav-item nav-link">Contacts</li></Link>
+            </ul>
+        </nav>
+    )
+}
