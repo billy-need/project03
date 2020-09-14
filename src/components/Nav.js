5621956a4a2a58f14fbccbf98bf0f51bd6064b87
Mike <mzeno184@gmail.com>
2020-09-13 23:13:34 -0400

    Updated navigation styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,21 +9,21 @@ export default function Nav() {
     return (
         <nav className="navbar navbar-expand-lg">
             <span className="navbar-brand">
-                <img src={logo} width="50" height="50" class="d-inline-block align-top" alt="" loading="lazy"></img>
+                <img src={logo} width="50" height="50" className="d-inline-block align-top" alt="" loading="lazy"></img>
                 <span className="mb-0 h1">Daily Planner</span>
             </span>
-            <button class="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+            <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarToggler">
                 <ul className="navbar-nav ml-auto">
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink exact className="nav-link" activeClassName="active" style={{ textDecoration: 'none' }} to="/">Home</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className="nav-link" activeClassName="active" style={{ textDecoration: 'none' }} to="/components/tasks">Tasks</NavLink>
                     </li>
-                    <li class="nav-item">
+                    <li className="nav-item">
                         <NavLink className="nav-link" activeClassName="active" style={{ textDecoration: 'none' }} to="/components/contacts">Contacts</NavLink>
                     </li>
                 </ul>
